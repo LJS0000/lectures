090fc6d7c3b834b059a64e52536bc204b0414256
LJS0000 <leejisu2281@gmail.com>
2024-10-08 15:45:25 +0900

    슈퍼코딩 강의_자바스크립트3_워들 헤더

---
+++ js/header.js
@@ -0,0 +1,7 @@
+const headerButtons = document.querySelectorAll('.header_btn');
+
+headerButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    alert('기능을 준비 중입니다');
+  });
+});
